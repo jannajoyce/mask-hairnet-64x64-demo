610087a77d1bfadecb6849b2b9f5f7a388ae8b16
jannajoyce <jannajoyce27@gmail.com>
2025-12-06 22:48:15 +0800

    first commit

---
+++ build/startup_max78000.d
@@ -0,0 +1,2 @@
+C:/MaximSDK/Examples/MAX78000/CNN/final-mask-hairnet-demo-64/build/startup_max78000.o: \
+ C:/analog/cfs/1.0.0/SDK/MAX/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
